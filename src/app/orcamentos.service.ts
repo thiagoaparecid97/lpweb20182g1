fdee1aabe4c48282f661daf2447e818e01385bf3
THIAGO APARECIDO DA SILVA <115930510@labin.canoas>
2018-10-01 18:48:28 -0300

    local Storage

--- src/app/orcamentos.service.ts
+++ src/app/orcamentos.service.ts
@@ -4,33 +4,40 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OrcamentosService {
-  orcamentos = [{
-    id: 1,
-    nome: 'Jackson',
-    email: 'jackson.souza@gmail.com',
-    pecas: [
-      {
-        tipo: 'compartimento de armário',
-        puxador: 'plástico',
-        pintura: 'acabamento PU',
-        largura: 1,
-        altura: 1,
-        profundidade: 1
-      },
-      {
-        tipo: 'gaveteiro',
-        puxador: 'metal',
-        pintura: 'acabamento PU texturizado',
-        largura: 15,
-        altura: 30,
-        profundidade: 70
-      }
-    ]
-  }];
-  constructor() { }
+  orcamentos = null;
+  constructor() {
+     this.orcamentos = JSON.parse(localStorage.getItem("orcamentos"));
+     if(this.orcamentos == null){
+      this.orcamentos = [{
+        id: 1,
+        nome: 'Jackson',
+        email: 'jackson.souza@gmail.com',
+        pecas: [
+          {
+            tipo: 'compartimento de armário',
+            puxador: 'plástico',
+            pintura: 'acabamento PU',
+            largura: 1,
+            altura: 1,
+            profundidade: 1
+          },
+          {
+            tipo: 'gaveteiro',
+            puxador: 'metal',
+            pintura: 'acabamento PU texturizado',
+            largura: 15,
+            altura: 30,
+            profundidade: 70
+          }
+        ]
+      }];
+      localStorage.setItem("orcamentos",JSON.stringify(this.orcamentos))
+     }
+   }
   salvar(orcamento) {
     orcamento.id = this.orcamentos.length + 1;
     this.orcamentos.push(orcamento);
+    localStorage.setItem("orcamentos",JSON.stringify(this.orcamentos))
   }
   calcular(orcamento) {
     let somatorio = 0;
